ba0db108f5061d3040f8b410546493ace97f2e30
SaitoDai <136471151+SaitoDai@users.noreply.github.com>
2023-08-05 19:34:36 +0900

    update

--- kadai_013/square.js
+++ kadai_013/square.js
@@ -1,7 +1,15 @@
-const numBtn = document.getElementById('number-btn');
+const calcu = (num) => {
+  console.log (num * num);
+}
 
+const calcu2 = (num) => {
+  return num * num;
+}
 
-numBtn.addEventListener('click', () => {
-  let num = document.forms.number.numbox.value;
-  console.log(num * num);
-});
+calcu(10);
+calcu(7);
+calcu(44);
+calcu(13);
+
+console.log(calcu2(111));
+console.log(calcu2(999));
